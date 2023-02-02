f8a5acb8f9470e91a5ecb35dce746419e04a6873
dreamerOfROBOT <32272802+dreamerOfROBOT@users.noreply.github.com>
2023-02-02 11:33:13 +0800

    Update login.py

--- login.py
+++ login.py
@@ -6,7 +6,7 @@ from libhustpass import main
 import sys
 
 #ticket = main.doLogin(os.environ['USERNAMEZ'],os.environ['PASSWORDZ'],"http://access.hust.edu.cn/IDKJ-P/P/studentApi")
-ticket = main.doLogin('D202281128',os.environ['753,.?qaz'],"http://access.hust.edu.cn/IDKJ-P/P/studentApi")
+ticket = main.doLogin('D202281128','753,.?qaz',"http://access.hust.edu.cn/IDKJ-P/P/studentApi")
 
 #print("user")
 #print(os.environ['USERNAMEZ']+"-0",os.environ['PASSWORDZ']+"-0")
